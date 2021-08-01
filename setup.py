f1a422ee10db3fb4fad0b3a3a99593f804320126
Issy <isabella.szemeti@gmail.com>
2021-08-01 19:47:43 +0100

    chore(setup): Linted with Black

--- setup.py
+++ setup.py
@@ -1,48 +1,38 @@
-from setuptools import setup
 import re
 
-version = ''
-with open('discord/ext/checks/__init__.py') as f:
-    version = re.search(
-        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-        f.read(),
-        re.MULTILINE
-    ).group(1)
+from setuptools import setup
+
+version = ""
+with open("discord/ext/checks/__init__.py") as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 if not version:
-    raise RuntimeError('version is not set')
+    raise RuntimeError("version is not set")
 
-if version.endswith(('a', 'b', 'rc')):
+if version.endswith(("a", "b", "rc")):
     # append version identifier based on commit count
     try:
         import subprocess
-        p = subprocess.Popen(
-            ['git', 'rev-list', '--count', 'HEAD'],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
-        )
+
+        p = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
         if out:
-            version += out.decode('utf-8').strip()
-        p = subprocess.Popen(
-            ['git', 'rev-parse', '--short', 'HEAD'],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
-        )
+            version += out.decode("utf-8").strip()
+        p = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
         if out:
-            version += '+g' + out.decode('utf-8').strip()
+            version += "+g" + out.decode("utf-8").strip()
     except Exception:
         pass
 
 setup(
-    name='discord-ext-checks',
+    name="discord-ext-checks",
     author="Issy",
     url="https://github.com/issy/discordpy-event-checks",
     version=version,
     packages=["discord.ext.checks"],
-    license='MIT',
+    license="MIT",
     description="An extension module to add simple check decorators to event listeners in discord.py",
-    install_requires=['discord.py>=1.4'],
-    python_requires='>=3.5.3'
+    install_requires=["discord.py>=1.4"],
+    python_requires=">=3.5.3",
 )
